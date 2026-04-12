888d394dc1f89dbfc09f036b5999815dfd08d38e
aline <145055918+alinec-santos@users.noreply.github.com>
2026-04-12 16:31:05 -0300

    Add: longest-substring-without-duplicates - submission-1

---
+++ Data Structures & Algorithms/longest-substring-without-duplicates/submission-1.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s): 
+        esquerdo = 0 
+        max_comprimento = 0
+        char_set = set()
+
+        for direito in range(len(s)):
+            while s[direito] in char_set:
+
+                char_set.remove(s[esquerdo])
+                esquerdo += 1 
+
+            char_set.add(s[direito])
+
+            janela_atual = direito - esquerdo + 1 
+
+            max_comprimento = max(max_comprimento,janela_atual)
+
+        return max_comprimento
+        
+
+#ESTRUTURA DE DADOS USADAS: SET()
+#ALGORITMO: JANELA DESLIZANTE
+#COMPLEXIDADE DE TEMPO: O (N)
+#COMPLEXIDADE DE ESPAÇO: O (N)
