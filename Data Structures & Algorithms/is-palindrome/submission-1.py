b6d3f72a38ca553b8dd21e7bbff5e21f86f753f1
aline <145055918+alinec-santos@users.noreply.github.com>
2026-04-12 12:02:25 -0300

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, s):
+        esquerdo,direito = 0, len(s)-1
+        while esquerdo<=direito:
+            while esquerdo <=direito and not s[esquerdo].isalnum():
+                    esquerdo += 1
+            while esquerdo <=direito and not s[direito].isalnum():
+                    direito -= 1 
+            if s[esquerdo].lower() != s[direito].lower():
+                return False
+            esquerdo += 1 
+            direito -= 1
+        return True
+
+
+#COMPLEXIDADE DE TEMPO: O(N)
+#COMPLEXIDADE DE ESPAÇO: O(N)
+
+#ESTRUTURA DE DADOS: STRING
+#ALGORITMO : TWO POINTERS
