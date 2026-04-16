8b46af41a617d04190db21be2fe6e74a869b0da8
aline <145055918+alinec-santos@users.noreply.github.com>
2026-04-16 08:32:22 -0300

    Add: depth-of-binary-tree - submission-1

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-1.py
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def maxDepth(self, root):
+        if not root:
+            return 0
+        
+        esquerda = maxDepth(root.left)
+        direita = maxDepth (root.right)
+
+        return 1 + max(esquerda, direita)
+        
+
+#Complexidades : 
+    #A complexidade de tempo é O(n) pois visitamos todos os nós. A complexidade de espaço é O(h), 
+ # onde h é a altura da árvore. No pior caso de uma árvore linear, isso degrada para O(n), 
+  #mas em uma árvore balanceada, o espaço é otimizado para O(log n) devido ao comportamento da pilha de recursão.
